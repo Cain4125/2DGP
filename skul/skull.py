8e06a83b786d605a097d3c3075affef03aa55077
park suhyeok <apato4125@gmail.com>
2025-11-19 00:31:20 +0900

    스컬 안정화

--- skul/skull.py
+++ skul/skull.py
@@ -1,4 +1,4 @@
-from pico2d import load_image, get_time, draw_rectangle
+from pico2d import load_image, get_time, draw_rectangle, clamp
 from sdl2 import SDL_KEYDOWN, SDL_KEYUP, SDLK_SPACE, SDLK_RIGHT, SDLK_LEFT, SDLK_a, SDLK_z, SDLK_x
 
 import game_world
@@ -406,7 +406,6 @@ class JumpAttack:
     def do(self):
         if 1 <= self.skull.frame <= 3:
             self.skull.check_attack_collision(self.hit_enemies)
-
         self.skull.recompute_dir()
         if self.skull.dir != 0:
             self.skull.face_dir = self.skull.dir
@@ -464,6 +463,9 @@ class Skull:
 
         self.world_w = world_w
         self.jump_count = 0
+
+        # [신규] 무적 시간 추가
+        self.invincible_timer = 0.0
 
         self.IDLE = Idle(self)
         self.RUN = Run(self)
@@ -545,7 +547,19 @@ class Skull:
                     o.take_damage(self.face_dir)
                     hit_enemies.append(id(o))
 
+    # [신규] 피격 함수 추가
+    def take_damage(self, attacker_pos_x):
+        if self.invincible_timer > 0.0:
+            return
+
+        print("SKULL HIT!")
+        self.invincible_timer = 1.5
+
     def update(self):
+        # [신규] 무적 시간 타이머 감소
+        if self.invincible_timer > 0.0:
+            self.invincible_timer -= game_framework.frame_time
+
         cur = self.state_machine.cur_state
         if cur not in (self.DASH, self.ATTACK1, self.ATTACK2):
             self.vy -= GRAVITY_PPS * game_framework.frame_time
@@ -555,6 +569,10 @@ class Skull:
         else:
             self.on_ground = False
         self.state_machine.update()
+
+        # [신규] 맵 밖으로 나가지 않게 고정
+        self.x = clamp(0 + self.half_w, self.x, self.world_w - self.half_w)
+
         cur_after_do = self.state_machine.cur_state
         if self.on_ground:
             if cur_after_do in (self.JUMP,):
@@ -594,9 +612,17 @@ class Skull:
             self.state_machine.handle_state_event(event_tuple)
 
     def draw(self, camera_x, camera_y):
-        self.state_machine.draw(camera_x, camera_y)
+        # [신규] 무적 시간 깜빡임 처리
+        if self.invincible_timer > 0.0:
+            if int(self.invincible_timer * 10) % 2 == 0:
+                pass
+            else:
+                self.state_machine.draw(camera_x, camera_y)
+        else:
+            self.state_machine.draw(camera_x, camera_y)
+
         lx, by, rx, ty = self.get_attack_bb()
-        # draw_rectangle(lx - camera_x, by - camera_y, rx - camera_x, ty - camera_y)
+        draw_rectangle(lx - camera_x, by - camera_y, rx - camera_x, ty - camera_y)
 
     def fire_ball(self):
         ball = Ball(self.x, self.y, self.face_dir * 10, self.world_w)
