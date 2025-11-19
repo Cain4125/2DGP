56651d7cfaf2af8cdc1b746ecfca187051f9c5bb
park suhyeok <apato4125@gmail.com>
2025-11-20 00:35:05 +0900

    배틀 스테이지 준비

--- skul/battle_stage.py
+++ skul/battle_stage.py
@@ -1,28 +1,50 @@
 import game_world
 from ground import Ground
+import camera
+from enemy_knight import EnemyKnight
+
+WORLD_WIDTH_PIXELS = 2000
+WORLD_HEIGHT_PIXELS = 800
 
 
 class BattleStage:
     def __init__(self, skull):
         self.skull = skull
-        self.ground = Ground(700, 30, 1400, 60)
+
+        # 1. 발판 뼈대
+        self.ground = Ground(WORLD_WIDTH_PIXELS // 2, 30, WORLD_WIDTH_PIXELS, 60)
+
         self.platforms = [self.ground]
-        print("BattleStage 생성됨")
+
+        self.knights = [EnemyKnight(600, 150, self.skull, self.platforms),
+                        #EnemyKnight(800, 210, self.skull, self.platforms)
+                        ]
+
+
 
     def enter(self):
         game_world.clear()
-        game_world.add_object(self.skull, 1)
+        game_world.add_object(self.skull, 2)
+
         for p in self.platforms:
             game_world.add_object(p, 0)
 
+        for e in self.knights:
+            game_world.add_object(e, 1)
+
+
         self.skull.platforms = self.platforms
+
         self.skull.x, self.skull.y = 100, 135
         self.skull.vy = 0
+
+        camera.camera.set_target_and_world(self.skull, WORLD_WIDTH_PIXELS, WORLD_HEIGHT_PIXELS)
 
     def exit(self):
         pass
 
     def update(self):
+        # (나중에 여기에 포탈 충돌 검사)
         return None
 
     def handle_events(self, event):
