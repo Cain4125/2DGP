0f5c106bb71f5c6f736fd87b548d07a81bc61a7c
park suhyeok <apato4125@gmail.com>
2025-12-07 23:00:32 +0900

    ui에 스킬 쿨타임

--- skul/ui.py
+++ skul/ui.py
@@ -7,6 +7,10 @@ class UI:
         self.skull = skull
         self.image_frame = load_image('Ui_frame.png')
         self.image_hp = load_image('hp_bar.png')
+        self.icon_a = load_image('A.png')
+        self.icon_a_cd = load_image('A_cooldown.png')
+        self.icon_s = load_image('S.png')
+        self.icon_s_cd = load_image('S_cooldown.png')
 
         self.w = 108
         self.h = 30
@@ -37,6 +41,20 @@ class UI:
                 hp_current_width,
                 hp_height
             )
+
+        skill_y = frame_bottom + (7 * SCALE) - 5
+        skill_a_x = frame_left + (48 * SCALE) + 38
+        skill_s_x = frame_left + (80 * SCALE) + 47
+        icon_size = self.icon_a.w * SCALE/1.7
+        if self.skull.skill_cooldown > 0:
+            self.icon_a_cd.draw(skill_a_x, skill_y, icon_size, icon_size)
+        else:
+            self.icon_a.draw(skill_a_x, skill_y, icon_size, icon_size)
+
+        if self.skull.skill_s_cooldown > 0:
+            self.icon_s_cd.draw(skill_s_x, skill_y, icon_size, icon_size)
+        else:
+            self.icon_s.draw(skill_s_x, skill_y, icon_size, icon_size)
 
     def get_bar_rect(self, x, y, w, h):
         return x, y, x + w, y + h
