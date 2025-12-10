b52441a0587661529b50a54349108841a348d2f2
park suhyeok <apato4125@gmail.com>
2025-12-10 17:21:05 +0900

    battlestage 완성

--- skul/battle_stage.py
+++ skul/battle_stage.py
@@ -1,24 +1,31 @@
-from pico2d import load_image, draw_rectangle
+from pico2d import load_image, draw_rectangle, load_font
 import game_world
 from ground import Ground
 import camera
+import game_framework
+from constants import SCALE
+import random
+
 from enemy_knight import EnemyKnight
+from enemy_greentree import EnemyGreenTree
 from enemy_tree import EnemyTree
 
-WORLD_WIDTH_PIXELS = 2000
-WORLD_HEIGHT_PIXELS = 800
+WORLD_WIDTH_PIXELS = 4000
+WORLD_HEIGHT_PIXELS = 1200
+
 
 class FixedBackground:
     def __init__(self):
-        self.image = load_image('sky.png')
+        self.image = load_image('sky2.png')
         self.image2 = load_image('mountain.png')
+        self.image3 = load_image('trees.png')
         self.w = WORLD_WIDTH_PIXELS
         self.h = WORLD_HEIGHT_PIXELS
 
     def draw(self, camera_x, camera_y):
         self.image.draw(self.w // 2 - camera_x, self.h // 2 - camera_y, self.w, self.h)
-
-        self.image2.draw(self.w // 2 - camera_x, self.h // 2 - camera_y, self.w, self.h)
+        self.image2.draw(self.w // 2 - camera_x, self.h // 2.5 - camera_y, self.w, self.h)
+        self.image3.draw(self.w // 2 - camera_x, self.h // 6 - camera_y, self.w, self.h)
 
     def update(self):
         pass
@@ -26,41 +33,112 @@ class FixedBackground:
     def get_bb(self):
         return 0, 0, 0, 0
 
+
+class Portal:
+    image_close = None
+    image_open = None
+
+    def __init__(self, x, y):
+        if Portal.image_close is None:
+            Portal.image_close = load_image('Portal_closed.png')
+        if Portal.image_open is None:
+            Portal.image_open = load_image('Portal_opened.png')
+        self.x, self.y = x, y
+        self.w, self.h = 176, 128
+        self.scale = SCALE
+
+        self.active = False
+        self.frame = 0
+        self.f_frame = 0.0
+        self.fps = 10.0
+        self.frame_count = 7
+
+    def update(self):
+        if self.active:
+            self.f_frame = (self.f_frame + self.fps * game_framework.frame_time) % self.frame_count
+            self.frame = int(self.f_frame)
+
+    def draw(self, camera_x, camera_y):
+        screen_x = self.x - camera_x
+        screen_y = self.y - camera_y
+        width = self.w * self.scale
+        height = self.h * self.scale
+
+        if self.active:
+            sx = self.frame * self.w
+            self.image_open.clip_draw(sx, 0, self.w, self.h, screen_x, screen_y, width, height)
+        else:
+            self.image_close.draw(screen_x, screen_y, width, height)
+
+    def activate(self):
+        self.active = True
+
+
 class BattleStage:
     def __init__(self, skull):
         self.skull = skull
 
-        self.ground = Ground(WORLD_WIDTH_PIXELS // 2, 30, WORLD_WIDTH_PIXELS, 60, is_main=True)
+        self.ground_left = Ground(700, 150, 1400, 300, is_main=True)
 
-        self.platforms = [self.ground]
+        self.ground_pit = Ground(2000, 50, 1200, 100, is_main=True)
 
-        self.knights = [EnemyKnight(600, 100, self.skull, self.platforms),
-                        EnemyKnight(1200, 100, self.skull, self.platforms)
-                        ]
+        self.ground_right = Ground(3300, 150, 1400, 300, is_main=True)
 
-        self.trees = [EnemyTree(900, 90, self.skull, self.platforms),
-                      EnemyTree(1500, 90, self.skull, self.platforms),]
+        self.spawn_platform = Ground(125, 650, 250, 40)
+
+        self.mid_platform = Ground(850, 450, 200, 40)
+
+        self.platforms = [
+            self.ground_left, self.ground_pit, self.ground_right,
+            self.spawn_platform, self.mid_platform
+        ]
+
+        self.enemies = []
+
+        # 1. Ground Left (Top Y=300): Knight 10마리
+        # X: 400 ~ 1300 사이에 배치
+        for i in range(10):
+            x = 400 + (i * 100) # 100 간격
+            y = 400 # 안전한 낙하 높이
+            self.enemies.append(EnemyKnight(x, y, self.skull, self.platforms))
+
+        # 2. Ground Pit (Top Y=100): Knight 5마리
+        # X: 1600 ~ 2400 사이에 배치
+        for i in range(5):
+            x = 1600 + (i * 200) # 200 간격
+            y = 200 # 안전한 낙하 높이
+            self.enemies.append(EnemyKnight(x, y, self.skull, self.platforms))
+
+        # 3. Ground Right (Top Y=300): Tree 1마리
+        self.enemies.append(EnemyTree(3300, 450, self.skull, self.platforms))
+
+
+        portal_x = 3500
+        portal_y = 300 + (128 * SCALE) / 2
+        self.portal = Portal(portal_x, portal_y)
 
         self.bg = FixedBackground()
 
     def enter(self):
         game_world.clear()
+
+        self.skull.world_w = WORLD_WIDTH_PIXELS
+
         game_world.add_object(self.bg, 0)
+
+        for e in self.enemies:
+            game_world.add_object(e, 1)
+
         game_world.add_object(self.skull, 2)
 
         for p in self.platforms:
             game_world.add_object(p, 0)
 
-        for e in self.knights:
-            game_world.add_object(e, 1)
-
-        for t in self.trees:
-            game_world.add_object(t, 1)
-
+        game_world.add_object(self.portal, 0)
 
         self.skull.platforms = self.platforms
 
-        self.skull.x, self.skull.y = 100, 135
+        self.skull.x, self.skull.y = 230, 750
         self.skull.vy = 0
 
         camera.camera.set_target_and_world(self.skull, WORLD_WIDTH_PIXELS, WORLD_HEIGHT_PIXELS)
@@ -69,7 +147,21 @@ class BattleStage:
         pass
 
     def update(self):
-        # (나중에 여기에 포탈 충돌 검사)
+        all_enemies_dead = True
+        for e in self.enemies:
+            if e.alive:
+                all_enemies_dead = False
+                break
+
+        if all_enemies_dead:
+            self.portal.activate()
+
+        portal_proximity_x = abs(self.skull.x - self.portal.x) < 50
+        is_up_pressed = self.skull.up_pressed
+
+        if portal_proximity_x and is_up_pressed and self.portal.active:
+            return 'boss_map'
+
         return None
 
     def handle_events(self, event):
