46322fbef4cf22e55d6e360b084ad8bbfb37c05e
park suhyeok <apato4125@gmail.com>
2025-12-10 14:24:16 +0900

    ui 코드수정

--- skul/ui.py
+++ skul/ui.py
@@ -21,40 +21,47 @@ class UI:
         self.x = 20 + (self.draw_w / 2)
         self.y = 20 + (self.draw_h / 2)
 
+        self.frame_left = self.x - (self.draw_w / 2) - 10
+        self.frame_bottom = self.y - (self.draw_h / 2)
+
+        self.hp_local_x = 38 * SCALE
+        self.hp_local_y = 17 * SCALE
+        self.hp_full_width = 70 * SCALE
+        self.hp_height = 8 * SCALE
+
+        self.skill_y = self.frame_bottom + (7 * SCALE) - 5
+        self.skill_a_x = self.frame_left + (48 * SCALE) + 38
+        self.skill_s_x = self.frame_left + (80 * SCALE) + 47
+        self.icon_size = self.icon_a.w * SCALE / 1.7
+
+        self.hp_current_width = 0
+        self.hp_draw_x = 0
+
     def update(self):
-        pass
+        hp_ratio = max(0, self.skull.current_hp / self.skull.max_hp)
+        self.hp_current_width = int(self.hp_full_width * hp_ratio)
+        self.hp_draw_x = self.frame_left + self.hp_local_x + (self.hp_current_width / 2)
 
     def draw(self, camera_x, camera_y):
-        self.image_frame.draw(self.x, self.y, self.w * SCALE, self.h * SCALE)
-        hp_local_x = 38 * SCALE
-        hp_local_y = 17 * SCALE
-        hp_full_width = 70 * SCALE
-        hp_height = 8 * SCALE
-        hp_ratio = max(0, self.skull.current_hp / self.skull.max_hp)
-        hp_current_width = int(hp_full_width * hp_ratio)
-        frame_left = self.x - (self.w * SCALE / 2) - 10
-        frame_bottom = self.y - (self.h * SCALE / 2)
-        if hp_current_width > 0:
+        self.image_frame.draw(self.x, self.y, self.draw_w, self.draw_h)
+
+        if self.hp_current_width > 0:
             self.image_hp.draw(
-                frame_left + hp_local_x + (hp_current_width / 2),
-                frame_bottom + hp_local_y,
-                hp_current_width,
-                hp_height
+                self.hp_draw_x,
+                self.frame_bottom + self.hp_local_y,
+                self.hp_current_width,
+                self.hp_height
             )
 
-        skill_y = frame_bottom + (7 * SCALE) - 5
-        skill_a_x = frame_left + (48 * SCALE) + 38
-        skill_s_x = frame_left + (80 * SCALE) + 47
-        icon_size = self.icon_a.w * SCALE/1.7
         if self.skull.skill_cooldown > 0:
-            self.icon_a_cd.draw(skill_a_x, skill_y, icon_size, icon_size)
+            self.icon_a_cd.draw(self.skill_a_x, self.skill_y, self.icon_size, self.icon_size)
         else:
-            self.icon_a.draw(skill_a_x, skill_y, icon_size, icon_size)
+            self.icon_a.draw(self.skill_a_x, self.skill_y, self.icon_size, self.icon_size)
 
         if self.skull.skill_s_cooldown > 0:
-            self.icon_s_cd.draw(skill_s_x, skill_y, icon_size, icon_size)
+            self.icon_s_cd.draw(self.skill_s_x, self.skill_y, self.icon_size, self.icon_size)
         else:
-            self.icon_s.draw(skill_s_x, skill_y, icon_size, icon_size)
+            self.icon_s.draw(self.skill_s_x, self.skill_y, self.icon_size, self.icon_size)
 
     def get_bar_rect(self, x, y, w, h):
         return x, y, x + w, y + h
